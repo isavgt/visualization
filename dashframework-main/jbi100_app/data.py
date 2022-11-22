6f5be4a577014f38293803e2101d6dbe4e5539bc
isavgt <isavogten@gmail.com>
2022-11-22 13:34:58 +0100

    Read in airbnb dataset

--- dashframework-main/jbi100_app/data.py
+++ dashframework-main/jbi100_app/data.py
@@ -1,10 +1,12 @@
+import pandas as pd
 import plotly.express as px
-
 
 def get_data():
     # Read data
     df = px.data.iris()
-
+    df_airbnb = pd.read_csv('Airbnb_Open_Data.csv')
     # Any further data preprocessing can go her
 
     return df
+
+get_data()
